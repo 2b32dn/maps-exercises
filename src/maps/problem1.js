2bfe0e0ff5f414238af85427e418c170bb15538f
jacques@decodemtl.com <jacques@decodemtl.com>
2017-12-10 13:01:10 -0500

    Maps exercises

---
+++ src/maps/problem1.js
@@ -0,0 +1,14 @@
+// Instructions: run this file like so:
+// node problem1.js
+
+var assert = require('assert');
+var age = { "jane": 28 }
+
+var both = {}
+
+for (var k in address) {
+    both[k] = { address: address[k], age: age[k] };
+}
+
+assert(both["jack"].age == 32)
+assert(both.jane["address"] == "123 elm street")
